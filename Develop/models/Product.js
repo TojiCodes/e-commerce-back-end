6451b21dcbc3a61e6d06681c9069ffaf60834991
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-17 21:11:18 -0500

    test

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,57 +1,23 @@
-// Imports Sequelize library components
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
-// Imports database connection
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Defines Product model by extending Sequelize's Model class
-class Product extends Model { }
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
 
-// Defines fields and rules for Product model
+// set up fields and rules for Product model
 Product.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-        unique: false
-      }
-    }
+    // define columns
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product'
+    modelName: 'product',
   }
 );
 
-// Exports Product model
 module.exports = Product;
