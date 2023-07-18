4f447effea18b6318bc4d4cb539fbc31f9906f84
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-17 21:10:41 -0500

    updated Category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,20 +1,36 @@
+// Imports required modules from Sequelize
 const { Model, DataTypes } = require('sequelize');
 
+// Imports the configured Sequelize instance
 const sequelize = require('../config/connection.js');
 
+// Creates a Category model extending the Sequelize Model class
 class Category extends Model {}
 
+// Initializes Category model
 Category.init(
   {
-    // define columns
+    // Defines model fields
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    // Model options
+    freezeTableName: true, // Prevents Sequelize from pluralizing the model name
+    timestamps: false, // Disables Sequelize's automatic timestamp fields (createdAt and updatedAt)
+    underscored: true, // Enables underscored instead of camel-cased column names
+    modelName: 'category', // Sets the model name
+    sequelize, // Passes the imported Sequelize connection instance
   }
 );
 
+// Exports the Category model
 module.exports = Category;
