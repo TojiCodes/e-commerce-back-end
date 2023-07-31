1cecd28a694f1d0a8040b8a70cd53ca0c3e927b4
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-31 12:38:41 -0500

    adjusted

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -24,11 +24,11 @@ Category.init(
   },
   {
     // Model options
+    sequelize, // Passes the imported Sequelize connection instance
     freezeTableName: true, // Prevents Sequelize from pluralizing the model name
     timestamps: false, // Disables Sequelize's automatic timestamp fields
     underscored: true, // Enables underscored instead of camel-cased column names
     modelName: 'category', // Sets the model name
-    sequelize, // Passes the imported Sequelize connection instance
   }
 );
 
