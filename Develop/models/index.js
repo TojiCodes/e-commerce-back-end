fdab40d9f6a0d34420919eaef7f460c9a7b679a8
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-31 12:44:37 -0500

    updated

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,28 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Defines relationships
+const associations = [
+  // Products belongsTo Category
+  { target: Category, source: Product, type: 'belongsTo' },
 
-// Categories have many Products
+  // Categories have many Products
+  { target: Product, source: Category, type: 'hasMany', options: { foreignKey: 'category_id' } },
 
-// Products belongToMany Tags (through ProductTag)
+  // Products belongToMany Tags (through ProductTag)
+  { target: Tag, source: Product, type: 'belongsToMany', options: { through: { model: ProductTag } } },
 
-// Tags belongToMany Products (through ProductTag)
+  // Tags belongToMany Products (through ProductTag)
+  { target: Product, source: Tag, type: 'belongsToMany', options: { through: { model: ProductTag } } },
+];
 
+// Applies associations
+associations.forEach(({ source, target, type, options }) => source[type](target, options));
+
+// Exports models
 module.exports = {
   Product,
   Category,
